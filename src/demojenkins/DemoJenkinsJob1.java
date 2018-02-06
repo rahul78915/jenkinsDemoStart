68207cd78a70423fd112466be14e4dbbec4baef7
rahul78915 <rahulprakash0862@gmail.com>
2018-02-06 10:46:05 +0530

    added  jenkins program

---
+++ src/demojenkins/DemoJenkinsJob1.java
@@ -0,0 +1,13 @@
+package demojenkins;
+
+import org.testng.annotations.Test;
+
+public class DemoJenkinsJob1 {
+	
+	@Test
+	public void testJenkins(){
+		
+		System.out.println("welcome to jenkins world");
+
+}
+}
